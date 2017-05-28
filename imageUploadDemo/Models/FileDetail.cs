80cf8d5c158959da10a83b6cc3091c231fff11b1
tmfahall <tmfahall@gmail.com>
2017-05-28 00:13:51 -0500

    working demo

---
+++ imageUploadDemo/Models/FileDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace imageUploadDemo.Models
+{
+
+    public class FileDetail
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+        public int SupportId { get; set; }
+        public virtual Support Support { get; set; }
+
+    }
+}
---
+++ imageUploadDemo/Models/SupportContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace imageUploadDemo.Models
+{
+
+    public class SupportContext : DbContext
+    {
+        public SupportContext()
+            : base("name=DefaultConnection")
+        {
+        }
+        public DbSet<Support> Supports { get; set; }
+        public DbSet<FileDetail> FileDetails { get; set; }
+    }
+}
